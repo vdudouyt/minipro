d5aba38c22a7d7b8d53f393a3e37e0288ab91d51
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2014-02-25 10:07:52 +0700

    AVR & PIC fuses support (missing files)

---
+++ fuses.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "fuses.h"
+
+fuse_decl_t avr_fuses[] = {
+	{ .name = "fuses_lo", .minipro_cmd = 0x12, .length = 1, .offset = 0 },
+	{ .name = "fuses_hi", .minipro_cmd = 0x12, .length = 1, .offset = 1 },
+	{ .name = "fuses_ext", .minipro_cmd = 0x12, .length = 1, .offset = 2 },
+	{ .name = "lock_byte", .minipro_cmd = 0x41, .length = 1, .offset = 0 },
+	{ .name = NULL },
+};
+
+fuse_decl_t pic_fuses[] = {
+	{ .name = "user_id0",  .minipro_cmd = 0x10, .length = 2, .offset = 0 },
+	{ .name = "user_id1",  .minipro_cmd = 0x10, .length = 2, .offset = 2 },
+	{ .name = "user_id2",  .minipro_cmd = 0x10, .length = 2, .offset = 4 },
+	{ .name = "user_id3",  .minipro_cmd = 0x10, .length = 2, .offset = 6 },
+	{ .name = "conf_word", .minipro_cmd = 0x12, .length = 2, .offset = 0 },
+	{ .name = NULL },
+};
---
+++ fuses.h
@@ -0,0 +1,14 @@
+#ifndef __FUSES_H
+#define __FUSES_H
+
+typedef struct fuse_decl {
+	const char *name;
+	char minipro_cmd;
+	char length;
+	char offset;
+} fuse_decl_t;
+
+extern fuse_decl_t avr_fuses[];
+extern fuse_decl_t pic_fuses[];
+
+#endif
