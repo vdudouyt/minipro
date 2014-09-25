vdudouyt/minipro

commit 09fc44b6838f23390fd1e4bf081005580389f5c0

Add support for disabling/enabling protection
The original software disables write protection before write and restores it
after write if the chip supports it. It provides a checkboxes for disabling the
functionality.

This changeset adds equivalent functionality with equivalent defaults. The bits
in opts4 were guessed by clicking around in the UI and seeing whether the
checkboxes grey out. The opcodes 0x44 and 0x45 and the transaction behavior
were identified by sniffing the traffic via NetPacket::USBMon.

minipro.h
@@ -23,6 +23,8 @@
 #define MP_PREPARE_WRITING 0x22
 #define MP_READ_CFG 0x12
 #define MP_WRITE_CFG 0x13
+#define MP_PROTECT_OFF 0x44
+#define MP_PROTECT_ON 0x45
 
 #define MAX_READ_BUFFER_SIZE 0x400
 #define MAX_WRITE_BUFFER_SIZE 0x210

main.c
@@ -17,6 +17,8 @@ struct {
 	device_t *device;
 	enum { UNSPECIFIED = 0, CODE, DATA, CONFIG } page;
         int erase;
+        int protect_off;
+        int protect_on;
 } cmdopts;
 
 void print_help_and_exit(const char *progname) {
@@ -26,6 +28,8 @@ void print_help_and_exit(const char *progname) {
 		"	-r <filename>	Read memory\n"
 		"	-w <filename>	Write memory\n"
 		"	-e 		Do NOT erase device\n"
+		"	-u 		Do NOT disable write-protect\n"
+		"	-P 		Do NOT enable write-protect\n"
 		"	-p <device>	Specify device\n"
 		"	-c <type>	Specify memory type (optional)\n"
 		"			Possible values: code, data, config\n"
@@ -58,10 +62,18 @@ void parse_cmdline(int argc, char **argv) {
 		print_help_and_exit(argv[0]);
 	}
 
-	while((c = getopt(argc, argv, "er:w:p:c:")) != -1) {
+	while((c = getopt(argc, argv, "euPr:w:p:c:")) != -1) {
 		switch(c) {
 		        case 'e':
 			  cmdopts.erase=1;  // 1= do not erase
+			  break;
+
+		        case 'u':
+			  cmdopts.protect_off=1;  // 1= do not disable write protect
+			  break;
+
+		        case 'P':
+			  cmdopts.protect_on=1;  // 1= do not enable write protect
 			  break;
 			  
 			case 'p':
@@ -365,6 +377,9 @@ void action_write(const char *filename, minipro_handle_t *handle, device_t *devi
 
 	minipro_begin_transaction(handle);
 	minipro_get_status(handle);
+	if (cmdopts.protect_off==0 && device->opts4 & 0xc000) {
+		minipro_protect_off(handle);
+	}
 
 	switch(cmdopts.page) {
 		case UNSPECIFIED:
@@ -383,6 +398,12 @@ void action_write(const char *filename, minipro_handle_t *handle, device_t *devi
 			break;
 	}
 	minipro_end_transaction(handle); // Let prepare_writing() to make an effect
+
+	if (cmdopts.protect_on==0 && device->opts4 & 0xc000) {
+		minipro_begin_transaction(handle);
+		minipro_protect_on(handle);
+		minipro_end_transaction(handle);
+	}
 }
 
 int main(int argc, char **argv) {

man/minipro.1
@@ -6,13 +6,13 @@ minipro \- flash various chips with Minipro TL866XX series of programmers from a
 .RB [-p " device"]
 .RB [-c " code|data|config"]
 .RB [-r|-w " filename"]
-.RB [-e]
+.RB [-e] [-u] [-P]
 
 .B miniprohex
 .RB [-p " device"]
 .RB [-c " code|data|config"]
 .RB [-r|-w " filename"]
-.RB [-e]
+.RB [-e] [-u] [-P]
 
 If
 .B -c

minipro.c
@@ -90,6 +90,18 @@ void minipro_end_transaction(minipro_handle_t *handle) {
 	msg_send(handle, msg, 4);
 }
 
+void minipro_protect_off(minipro_handle_t *handle) {
+	memset(msg, 0, sizeof(msg));
+	msg_init(msg, MP_PROTECT_OFF, handle->device);
+	msg_send(handle, msg, 10);
+}
+
+void minipro_protect_on(minipro_handle_t *handle) {
+	memset(msg, 0, sizeof(msg));
+	msg_init(msg, MP_PROTECT_ON, handle->device);
+	msg_send(handle, msg, 10);
+}
+
 int minipro_get_status(minipro_handle_t *handle) {
 	unsigned char buf[32];
 	msg_init(msg, MP_REQUEST_STATUS1_MSG2, handle->device);

commit ea6356cc26f6559d11a80943fd965411e4b380d9

Add two missing prototypes
This avoids compiler warnings. I've broke this in
[09fc44b68]: Add support for disabling/enabling protection

minipro.h
@@ -49,6 +49,8 @@ minipro_handle_t *minipro_open(device_t *device);
 void minipro_close(minipro_handle_t *handle);
 void minipro_begin_transaction(minipro_handle_t *handle);
 void minipro_end_transaction(minipro_handle_t *handle);
+void minipro_protect_off(minipro_handle_t *handle);
+void minipro_protect_on(minipro_handle_t *handle);
 int minipro_get_status(minipro_handle_t *handle);
 void minipro_read_block(minipro_handle_t *handle, unsigned int type, unsigned int addr, unsigned char *buf);
 void minipro_write_block(minipro_handle_t *handle, unsigned int type, unsigned int addr, unsigned char *buf);