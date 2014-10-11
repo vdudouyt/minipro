79cb420393e4ede48a10ed7ed8eb5cce5543231a
Lubomir Rintel <lkundrak@v3.sk>
2014-10-11 08:01:20 +0200

    Fix -I option in usage info

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ void print_help_and_exit(const char *progname) {
 		"	-p <device>	Specify device\n"
 		"	-c <type>	Specify memory type (optional)\n"
 		"			Possible values: code, data, config\n"
-		"	-i		Use ICSP\n";
+		"	-i		Use ICSP\n"
 		"	-I		Use ICSP (without enabling Vcc)\n";
 	fprintf(stderr, usage, progname);
 	exit(-1);
